b6f45b41b62e5f34a605d2b071a6609ab6649e3a
Ruben Brinkman <github@brnkmn.com>
2023-04-17 02:14:57 +0200

    Added Sass + basic example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,46 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Show from '$lib/Show.svelte';
+	import type { Step } from '$lib/types';
+	import { onMount } from 'svelte';
+
+	let steps: Array<Step> = [];
+	let shown = false;
+
+	onMount(() => {
+		steps = [
+			{
+				title: 'Here you define a title for the modal',
+				body: 'Here you define a body for the modal',
+				target: document.getElementById('title')
+			},
+			{
+				title: 'First paragraph',
+				body: 'Here you define a body for the modal',
+				target: document.getElementById('first')
+			},
+			{
+				title: 'Second paragraph',
+				body: 'Here you define a body for the modal',
+				target: '#second'
+			}
+		];
+
+		shown = true;
+	});
+</script>
+
+<h1 id="title">Welcome to your library project</h1>
+<p id="first">
+	Create your package using @sveltejs/package and preview/showcase your work with SvelteKit
+</p>
+<p id="second">
+	Visit <a class="link" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Show id={'test-show'} {steps} {shown} />
+
+<style lang="scss">
+	#title {
+		color: red;
+	}
+</style>
